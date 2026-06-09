fe07114bfd08a654871aa195e88449dafe93c065
Dev Zero <litkaponii@gmail.com>
2026-06-08 21:28:55 -0500

    Creación inicial del proyecto API Auth JWT

---
+++ src/main/java/com/rportaldev/apiauthjwt/ApiAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package com.rportaldev.apiauthjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAuthJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rportaldev/apiauthjwt/ApiAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rportaldev.apiauthjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
