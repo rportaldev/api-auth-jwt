fb82e565446d9c72abf68289f9454910a103d759
Dev Zero <litkaponii@gmail.com>
2026-06-09 20:08:03 -0500

    Creación de paquetes, entidad Usuario y enum Rol

---
+++ src/main/java/com/rportaldev/apiauthjwt/entity/Usuario.java
@@ -0,0 +1,54 @@
+package com.rportaldev.apiauthjwt.entity;
+
+import java.time.LocalDateTime;
+
+import com.rportaldev.apiauthjwt.enums.Rol;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nombre",
+			nullable = false)
+	private String nombre;
+	
+	@Column(name = "correo",
+			nullable = false,
+			unique = true)
+	private String correo;
+	
+	@Column(name = "password",
+			nullable = false)
+	private String password;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "rol", 
+			nullable = false)
+	private Rol rol;
+	
+	@Column(name = "fecha_registro",
+			nullable = false)
+	private LocalDateTime fechaRegistro;
+}
---
+++ src/main/java/com/rportaldev/apiauthjwt/enums/Rol.java
@@ -0,0 +1,7 @@
+package com.rportaldev.apiauthjwt.enums;
+
+public enum Rol {
+
+	ROLE_ADMIN,
+    ROLE_USER
+}
