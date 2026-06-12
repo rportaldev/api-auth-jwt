0c985025a54af85c807b36ae46d4f9b7419df15d
Dev Zero <litkaponii@gmail.com>
2026-06-11 19:37:30 -0500

    Implementación de manejo global de excepciones

---
+++ src/main/java/com/rportaldev/apiauthjwt/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.rportaldev.apiauthjwt.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(RecursoNoEncontradoException.class)
+	public ResponseEntity<String> handlerRecursoNoEncontrado(
+			RecursoNoEncontradoException ex) {
+		return ResponseEntity
+				.status(HttpStatus.NOT_FOUND)
+				.body(ex.getMessage());
+	}
+}
