18ac2fcd2cb9dd0084ac1e5ba70de462635aa2d2
Dev Zero <litkaponii@gmail.com>
2026-07-06 20:51:42 -0500

    feat: agregar UsuarioResponseDTO

---
+++ src/main/java/com/rportaldev/apiauthjwt/dto/UsuarioResponseDTO.java
@@ -0,0 +1,21 @@
+package com.rportaldev.apiauthjwt.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UsuarioResponseDTO {
+
+	private Long id;
+	private String nombre;
+	private String correo;
+	private String rol;
+	private LocalDateTime fechaRegistro;
+}
